56c91be20d84055d4ee2f690ca7ee3d004fb4146
TWO-ICE <115453069+TWO-ICE@users.noreply.github.com>
2024-12-23 13:36:37 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1653b0c7bd7180ccadb0caa2c6abb00a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '二冰',
+  domain: 'inbox.ebeb.fun',
+  author: 'erbing',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '二冰',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
